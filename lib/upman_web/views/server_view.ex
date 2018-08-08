39bdd43c7c1f58446c48ce34474e3d83bd45f113
Moritz Bellach <moritzbellach@gmail.com>
2018-08-08 19:03:52 +0200

    refactor server (overview)view

--- lib/upman_web/views/server_view.ex
+++ lib/upman_web/views/server_view.ex
@@ -1,3 +1,25 @@
 defmodule UpmanWeb.ServerView do
   use UpmanWeb, :view
+
+  def data_present(server, name) do
+    data = Upman.Data.server(server)
+    data[name] != nil && data[name] != []
+  end
+
+  def linkserver(name) do
+    target = cond do
+      data_present(name, "updates") -> "#updates"
+      data_present(name, "locked") -> "#locked"
+      data_present(name, "os") -> "#osrelease"
+      data_present(name, "customfacts") -> "#customfacts"
+      true -> ""
+    end
+    link name, to: "/servers/" <> name <> target
+  end
+
+  def timetag(time) do
+    timestr = DateTime.to_string(time)
+    human_time = (timestr |> String.split(":") |> Enum.take(2) |> Enum.join(":")) <> " UTC"
+    content_tag :time, human_time, datetime: timestr
+  end
 end

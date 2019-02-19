132334972f998d3cc30b727c90618d9dcab61b63
Moritz Bellach <moritzbellach@gmail.com>
2019-02-19 14:51:34 +0100

    formatting only

--- lib/upman_web/controllers/session_controller.ex
+++ lib/upman_web/controllers/session_controller.ex
@@ -2,27 +2,36 @@ defmodule UpmanWeb.SessionController do
   use UpmanWeb, :controller
 
   def new(conn, _params) do
-    render conn, "login.html", token: get_csrf_token()
+    render(conn, "login.html", token: get_csrf_token())
   end
 
   def create(conn, %{"username" => username, "password" => password}) do
     if auth(username, password) do
-      conn |> put_session(:logged_in, :true) |> put_session(:user, username) |> put_flash(:info, "Access granted") |> redirect(to: server_path(conn, :index))
+      conn
+      |> put_session(:logged_in, true)
+      |> put_session(:user, username)
+      |> put_flash(:info, "Access granted")
+      |> redirect(to: server_path(conn, :index))
     else
       conn |> put_flash(:error, "Access Denied") |> redirect(to: session_path(conn, :new))
     end
   end
 
   def auth(username, password) do
-    with  {:ok, ldap} <- Exldap.open,
-          {:ok, [entry]} <- Exldap.search_field(ldap, Application.fetch_env!(:exldap, :userAttr), username),
-          dn <- entry.object_name
-    do
+    with {:ok, ldap} <- Exldap.open(),
+         {:ok, [entry]} <-
+           Exldap.search_field(ldap, Application.fetch_env!(:exldap, :userAttr), username),
+         dn <- entry.object_name do
       try do
         :ok = Exldap.verify_credentials(ldap, dn, password)
-        Exldap.search_field(ldap, Application.fetch_env!(:exldap, :groupAttr), Application.fetch_env!(:exldap, :group))
+
+        Exldap.search_field(
+          ldap,
+          Application.fetch_env!(:exldap, :groupAttr),
+          Application.fetch_env!(:exldap, :group)
+        )
         |> elem(1)
-        |> List.first
+        |> List.first()
         |> Exldap.get_attribute!(Application.fetch_env!(:exldap, :memberAttr))
         |> Enum.member?(username)
       rescue
@@ -31,7 +40,6 @@ defmodule UpmanWeb.SessionController do
     else
       _ -> {:error, "authentication failed"}
     end
-
   end
 
   def delete(conn, _params) do

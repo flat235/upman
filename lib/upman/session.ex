1125cf2ca1f337033e38abd13e59801ba3429d75
Moritz Bellach <moritzbellach@gmail.com>
2019-02-20 20:19:20 +0100

    make dialyzer even happier

--- lib/upman/session.ex
+++ lib/upman/session.ex
@@ -1,6 +1,7 @@
 defmodule Upman.Session do
+  @spec logged_in(Plug.Conn.t()) :: boolean()
   def logged_in(conn) do
-    Plug.Conn.get_session(conn, :logged_in)
+    !!Plug.Conn.get_session(conn, :logged_in)
   end
 
   def user(conn) do
